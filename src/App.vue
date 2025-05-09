3b3ad2193fae54fc3c292d5ac8e5bb1ef9010914
qadrihafyan <qadrihafyan03@gmail.com>
2025-05-09 15:41:45 +0700

    input task and function add task

--- src/App.vue
+++ src/App.vue
@@ -18,18 +18,39 @@ const tasks = ref([
     completed: false
   }
 ])
+const newTask = ref('')
+
+const addTask = () => {
+  if (newTask.value === '') {
+    return
+  }
+  tasks.value.push({
+    id: tasks.value.length + 1,
+    title: newTask.value,
+    completed: false
+  })
+  newTask.value = ''
+  console.log(tasks.value)
+}
 </script>
 
 <template>
   <div>
-    <ul>
-      <li v-for="task in tasks" :key="task.id">
-        {{ task.title }} - {{ task.completed }}
-      </li>
-    </ul>
+    <!-- Left Content -->
+    <div>
+      <input type="text" v-model="newTask" @keyup.enter="addTask" />
+      <button @click="addTask">Add Task</button>
+    </div>
+
+    <!-- Right Content -->
+    <div>
+      <ul>
+        <li v-for="task in tasks" :key="task.id">
+          {{ task.title }} - {{ task.completed }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
